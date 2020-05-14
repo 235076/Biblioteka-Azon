0e58c01f4f6eb31c14fb9fe8e76afd0645805950
msikorski235076 <235076@student.pwr.edu.pl>
2020-05-14 17:03:58 +0200

    v0.1

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Biblioteka-Azon',
+    version='0.1',
+    packages=find_packages(exclude=['tests*']),
+    license='MIT',
+    description='bilbioteka umozliwiajaca prace z zasbami azon',
+    url='https://git.e-science.pl/msikorski235076_dpp/python_pip',
+    author='Mikolaj Sikorski',
+    author_email='235076@student.pwr.edu.pl'
+)
